cf110b1d6f35f7e27224bf436214458cd4e29716
Mr Surya <123524250+mrvsurya@users.noreply.github.com>
2024-03-21 13:05:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ def to_hms(seconds: int) -> list:
     ---------
     list
         a list of integers representing hours, minutes, seconds
+
+    Example:
+    >>> to_hms(10)
+    [0, 0, 10]
+    >>> to_hms(61)
+    [0, 1, 1]
+    >>> to_hms(7199)
+    [1, 59, 59]
     """
     # Type your code below
     pass
